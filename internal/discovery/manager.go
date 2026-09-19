9fd037e5ab202fdc25bc2db266060ffdea256799
Somnath Mukherjee <somnathbm0@gmail.com>
2026-09-19 16:08:58 +0530

    discovery/manager - initial code change

--- internal/discovery/manager.go
+++ internal/discovery/manager.go
@@ -2,16 +2,35 @@ package discovery
 
 import (
 	"context"
-
-	"github.com/somnathbm/horus/internal/config"
+	"fmt"
 )
 
 type Manager struct {
 	discoverers []Discoverer
 }
 
-func New(config config.DiscoveryConfig) {
-
+func New(discoverers ...Discoverer) Manager {
+	return Manager{discoverers: discoverers}
 }
 
-func (m *Manager) Discover(ctx context.Context) (DiscoveryResult, error)
+func (m Manager) Discover(ctx context.Context) (DiscoveryResult, error) {
+	var result DiscoveryResult
+
+	for _, discoverer := range m.discoverers {
+		dscvryResult, dscvryErr := discoverer.Discover(ctx)
+		// context cancelled
+		if err := ctx.Err(); err != nil {
+			return result, fmt.Errorf("discovery manager: %w", err)
+		}
+		// process level error
+		if dscvryErr != nil {
+			return result, fmt.Errorf("discovery manager: %w", dscvryErr)
+		}
+
+		result.Targets = append(result.Targets, dscvryResult.Targets...)
+		result.Failures = append(result.Failures, dscvryResult.Failures...)
+	}
+
+	// result can be enriched later for reporting
+	return result, nil
+}
